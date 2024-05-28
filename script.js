bdee90d1007f177953dc8ed75625c9dde391a955
Favian David Martinez <117396405+fabian-marthin@users.noreply.github.com>
2024-05-28 12:24:10 -0500

    Se agrega la sección de proyectos y contactos

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-console.log("hola")
+const sobreMi = document.querySelector(".sobreMi");
+sobreMi.addEventListener("click",()=>{
+    console.log("hola1");
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
 
-const btn = document.getElementById("btnCurri");
-btn.addEventListener("click", abrirCurriculum);
+const diplomas = document.querySelector(".diplomas");
+diplomas.addEventListener("click", ()=>{
+    console.log("hola2");
+    window.scrollTo({
+        top: 740,
+        behavior: 'smooth'
+    });
+})
+const proyectos = document.querySelector(".proyectos");
+proyectos.addEventListener("click", ()=>{
+    console.log("hola3");
+    window.scrollTo({
+        top: 2720,
+        behavior: 'smooth'
+    });
+})
+const contacto = document.querySelector(".contacto");
+contacto.addEventListener("click", ()=>{
+    console.log("hola4");
+    window.scrollTo({
+        top: 5000,
+        behavior: 'smooth'
+    });
+})
 
+
+/* function scrollToPosition(targetPosition) {
+    window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+    });
+} */
+
+
+const btnDesc = document.getElementById("btnCurri");
+btnDesc.addEventListener("click", abrirCurriculum);
 function abrirCurriculum (){
-    const link =document.createElement("a");
-    link.href = "./imagenees/HV Front end Developer.pdf";
-    link.click;
-
     window.open("./imagenees/HV Front end Developer.pdf")
 }
+
+const btnYoutube = document.querySelector(".iconoYoutube");
+btnYoutube.addEventListener("click", abrirYouTube);
+function abrirYouTube(){
+    window.open("https://www.youtube.com/watch?v=f6Ynda2Jpao&t=50s")
+}
+
+const whatsapp = document.querySelector(".whatsapp");
+whatsapp.addEventListener("click", abrirWhatsApp);
+function abrirWhatsApp(){
+    window.open("https://wa.link/koyc47")
+}
+
+const github = document.querySelector(".github");
+github.addEventListener("click", abrirgithub);
+function abrirgithub(){
+    window.open("https://github.com/fabian-marthin")
+}
+
+const linkedin = document.querySelector(".linkedin");
+linkedin.addEventListener("click", abrirlinkedin);
+function abrirlinkedin(){
+    window.open("https://www.linkedin.com/in/fabian-david-martinez-martin-80a455224/?trk=opento_sprofile_details")
+}
+
+
